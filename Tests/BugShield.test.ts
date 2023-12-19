0c085a1fd8f9a8bcfbffb2436dbd55f7ca2016c7
H4ckW1s3r <clementix01@gmail.com>
2023-12-19 13:59:36 +0100

    Added tests cases

--- Tests/BugShield.test.ts
+++ Tests/BugShield.test.ts
@@ -29,10 +29,19 @@ describe('BugShield', () => {
   it('should create a custom error', () => {
     const error = bugShield.createError('CustomError', 'This is a custom error', '123');
 
-    expect(error).toBeInstanceOf(BugShieldError);
+    expect(error.name).toBeDefined();
+    expect(error.message).toBeDefined();
+    expect(error.code).toBeDefined();
     expect(error).toHaveProperty('name', 'CustomError');
     expect(error).toHaveProperty('message', 'This is a custom error');
     expect(error).toHaveProperty('code', '123');
+  });
+
+  it('should be instance of BugShieldError and Error', () => {
+    const error = bugShield.createError('CustomError', 'This is a custom error', '123');
+
+    expect(error).toBeInstanceOf(BugShieldError);
+    expect(error).toBeInstanceOf(Error);
   });
 
   // Add more test cases for handleError, getLogger, etc.

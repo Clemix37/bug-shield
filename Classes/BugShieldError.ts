c2a55e674bbc273a67637b644f0ff112e0ddd7a1
H4ckW1s3r <clementix01@gmail.com>
2023-12-20 18:59:04 +0100

    Update BugShieldError regions

--- Classes/BugShieldError.ts
+++ Classes/BugShieldError.ts
@@ -1,9 +1,16 @@
 import IBugShieldError from "../Interfaces/IBugShieldError";
 
 export default class BugShieldError extends Error implements IBugShieldError {
+
+    //#region Properties
+
     name: string;
     code?: string | number;
     context?: Record<string, any>;
+
+    //#endregion
+
+    //#region Constructor
   
     constructor(name: string, message: string, code?: string | number, context?: Record<string, any>) {
         super(message);
@@ -13,5 +20,8 @@ export default class BugShieldError extends Error implements IBugShieldError {
         // Ensure the stack trace is captured.
         Error.captureStackTrace(this, BugShieldError);
     }
-  }
+    
+    //#endregion
+
+}
   

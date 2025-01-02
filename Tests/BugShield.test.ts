fae07a45920c86cf3e8f4c8cedfbf14eb2c5e5ce
H4ckW1s3r <37782019+Clemix37@users.noreply.github.com>
2025-01-02 08:40:13 +0100

    Update BugShield.test.ts with comments

--- Tests/BugShield.test.ts
+++ Tests/BugShield.test.ts
@@ -6,12 +6,13 @@ import IBugShieldConfig from "../Interfaces/IBugShieldConfig";
 describe("BugShield", () => {
     let bugShield: BugShield;
 
+    // Before each test, create and store an instance of BugShield
     beforeEach(() => {
         bugShield = new BugShield();
     });
 
     it("should create an instance of BugShield", () => {
-        expect(bugShield).toBeInstanceOf(BugShield);
+        expect(bugShield).toBeInstanceOf(BugShield); // Check for instance
     });
 
     it("should configure BugShield with provided config", () => {
@@ -24,7 +25,7 @@ describe("BugShield", () => {
 
         bugShield.configure(config);
 
-        expect(BugShield.config).toEqual(config);
+        expect(BugShield.config).toEqual(config); // Check for correct configuration
     });
 
     it("should create a custom error", () => {
@@ -34,12 +35,14 @@ describe("BugShield", () => {
             "123",
         );
 
+        // Each property is defined
         expect(error.name).toBeDefined();
         expect(error.message).toBeDefined();
         expect(error.code).toBeDefined();
-        expect(error).toHaveProperty("name", "CustomError");
-        expect(error).toHaveProperty("message", "This is a custom error");
-        expect(error).toHaveProperty("code", "123");
+        // Each property is correctly assigned
+        expect(error).toHaveProperty("name", "CustomError"); // Name is assigned the correct value
+        expect(error).toHaveProperty("message", "This is a custom error"); // Message is assigned the correct value
+        expect(error).toHaveProperty("code", "123"); // Code is assigned the correct value
     });
 
     it("should be instance of BugShieldError and Error", () => {
@@ -49,13 +52,13 @@ describe("BugShield", () => {
             "123",
         );
 
-        expect(error).toBeInstanceOf(BugShieldError);
-        expect(error).toBeInstanceOf(Error);
+        expect(error).toBeInstanceOf(BugShieldError); // Returns BugShieldError
+        expect(error).toBeInstanceOf(Error); // BugShieldError extends Error
     });
 
     it("should get a logger and be instance of Logger", () => {
         const newLogger = bugShield.getLogger("test-logger");
-        expect(newLogger).toBeInstanceOf(Logger);
+        expect(newLogger).toBeInstanceOf(Logger); // Check for the instance of Logger
     });
 
     // Add more test cases for handleError, etc.
